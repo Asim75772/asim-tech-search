757b86dd25da41ceda98f37ea98f954b6a1f2b39
Asim75772 <146651181+Asim75772@users.noreply.github.com>
2026-09-08 00:42:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const CACHE_NAME="asim-tech-search-v7";
-const FILES=["./","./index.html","./mountain-bg.png","./manifest.json"];
+const CACHE_NAME="asim-tech-search-v8";
+const FILES=["./","./index.html","./mountain-bg.png","./manifest.json","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match("./index.html"))));
-});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match("./index.html"))))});
