3f7ed63de553c42d7a7cda54ccb6b938d1939a51
Asim75772 <146651181+Asim75772@users.noreply.github.com>
2026-09-08 00:28:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="asim-tech-search-v6";
+const CACHE_NAME="asim-tech-search-v7";
 const FILES=["./","./index.html","./mountain-bg.png","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
